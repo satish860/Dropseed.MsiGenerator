f0d153b35fdf8417ac56b96fc6b715a596f90623
satish <satish860@gmail.com>
2016-07-18 13:08:45 +0530

    Initial Check which can generate MSI based on the Name of the project

---
+++ Dropseed.MsiGenerator.Tests/MsiGeneratorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Should;
+
+namespace Dropseed.MsiGenerator.Tests
+{
+    public class MsiGeneratorTests
+    {
+        public void ShouldBeAbleToCreateMSIWithJustProvidingName()
+        {
+            MsiGenerator generator = new MsiGenerator();
+            string path=generator.Build("Dropseed.minions");
+            path.ShouldNotBeNull();
+        }
+    }
+}
---
+++ Dropseed.MsiGenerator/MsiGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WixSharp;
+
+namespace Dropseed.MsiGenerator
+{
+    public class MsiGenerator
+    {
+        public string Build(string name)
+        {
+            Project project = new Project(name);
+            return Compiler.BuildMsi(project);
+        }
+    }
+}
