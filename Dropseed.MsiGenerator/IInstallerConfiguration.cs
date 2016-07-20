5f44adcf757865caee1eb4f8401f49a6300f7241
satish <satish860@gmail.com>
2016-07-20 18:07:52 +0530

    MSI Builder for commit

---
+++ Dropseed.MsiGenerator/IInstallerConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WixSharp;
+
+namespace Dropseed.MsiGenerator
+{
+    public interface IInstallerConfiguration
+    {
+        object Configure(MsiBuilder builder);
+
+        Project BuildProject(object ConfigurationBuilder);
+    }
+
+}
---
+++ Dropseed.MsiGenerator/InstallerConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using WixSharp;
+
+namespace Dropseed.MsiGenerator
+{
+    public abstract class InstallerConfiguration<T> : IInstallerConfiguration
+    {
+        public object Configure(MsiBuilder builder)
+        {
+            return AddConfiguration(builder);
+        }
+
+        public Project BuildProject(object ConfigurationBuilder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract T AddConfiguration(MsiBuilder builder);
+
+        public abstract Project BuildProject(T builder);
+        
+    }
+}
